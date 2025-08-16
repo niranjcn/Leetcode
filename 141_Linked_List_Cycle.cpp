465d177bbb9d4feffa0a40b5345ee98e7ce20d2e
Niranj <niranjcn024@gmail.com>
2025-08-16 21:51:49 +0530

    141

---
+++ 141_Linked_List_Cycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head)
+        {
+            return false;
+        }
+        map<ListNode*,bool>visited;
+        while(head)
+        {
+            if(visited[head]==true)
+            {
+                return true;
+            }
+            else{
+                visited[head]=true;
+                head=head->next;
+            }
+        }
+        return false;
+    }
+};
