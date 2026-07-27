9e7854eb5e4c72e249f9f01eaa6991f4530968d7
Niranj C N <122597624+niranjcn@users.noreply.github.com>
2026-07-27 10:36:24 +0530

    Update 110. Balanced Binary Tree

--- 110_Balanced_Binary_Tree.py
+++ 110_Balanced_Binary_Tree.py
@@ -1,20 +1,18 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
+
         def dfs(node):
             if not node:
-                return 0
-        
-            left = dfs(node.left)
-            right = dfs(node.right)
-            if left == -1:
-                return -1
-            if right == -1:
-                return -1
-            if abs(left-right) > 1:
-                return - 1
-            else:
-                return 1 + max(left,right)
-        
-        res = dfs(root)
-        return res != -1
+                return 0,True
+            
+            left,l_bal = dfs(node.left)
+            right,r_bal = dfs(node.right)
+            
+            height =  1 + max(left,right)
+
+            if not l_bal or not r_bal or abs(left-right) > 1:
+                return height, False
+            
+            return height, True
+        height, balanced = dfs(root)
+        return balanced
