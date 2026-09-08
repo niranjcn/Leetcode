1c190b29062b1c189096706f7c5b974ad5713973
Niranj C N <122597624+niranjcn@users.noreply.github.com>
2026-09-08 11:17:50 +0530

    Update 540. Single Element in a Sorted Array

---
+++ 540_Single_Element_in_a_Sorted_Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l,r = 0,len(nums)-1
+
+        while l < r:
+            mid = (l + r)//2
+
+            if (mid % 2 != 0 and nums[mid] == nums[mid-1]) or (mid % 2 == 0 and nums[mid] == nums[mid+1]):
+                l = mid + 1
+            else:
+                r = mid
+
+        return nums[l]
